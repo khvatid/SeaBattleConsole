e5f6680cc1db8d25a3ad74d40bc3ec4025167058
Dmitry Khvatalov <hv20027@gmail.com>
2021-05-16 22:49:04 +0300

    After plan server

--- server/ConsoleApp4/Program.cs
+++ server/ConsoleApp4/Program.cs
@@ -12,7 +12,7 @@ namespace ServerForBattleShip
         static List<TcpClient> clients = new List<TcpClient>();
         static string data = null;
         static byte[] b = new byte[256];
-        static bool allhere = false;
+        private static int log = 0;
         private static void Main(string[] args)
         {
             tcpListener.Start();
@@ -28,39 +28,73 @@ namespace ServerForBattleShip
                         j++;
                         Console.WriteLine($"user {j} connect ");
                         NetworkStream sender = client.GetStream();
-                        Byte[] _data = System.Text.Encoding.Unicode.GetBytes("привет пидорас");
+                        Byte[] _data = System.Text.Encoding.Unicode.GetBytes("waiting game");
                         sender.Write(_data, 0, _data.Length);
                         clients.Add(client);
                     }
-
-                    if (!allhere)
-                        try
-                        {
-
-                            if (clients[0].Connected && clients[1].Connected)
-                            {
-                                allhere = true;
-                                SendToAllClients("player found");
-                            }
-                        }
-                        catch
-                        {
-                            SendToAllClients("ждем второго гандона");
-                        }
-
-                    // if (allhere)
-                    ReciveFromClients(client);
+                    if (clients.Count == 2)
+                    {
+                        SendToAllClients("player found");
+                        PlayRoom(client);
+                    }
+                    else
+                    {
+                        AwaitRoom(client);
+                    }
+                        
+                    //ReciveFromClients(client);
                 });
             }
         }
 
-        static async private void AwaitRoom()
+        static async private void AwaitRoom(TcpClient client)
         {
             await Task.Factory.StartNew(() =>//отправка всем клиентам
             {
+                bool wait = true;
+                while(wait)
+                {
+                    if(clients.Count == 2)
+                    {
+                        wait = false;
+                        
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+                PlayRoom(client);
             
             });
         }
+
+
+        static async private void PlayRoom(TcpClient client)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                NetworkStream stream = client.GetStream();
+                Byte[] vs = new Byte[10];
+                stream.Read(vs, 0, 10);
+                string data = System.Text.Encoding.Unicode.GetString(vs, 0, 10);
+                if (data == "done")
+                {
+                    log++;
+                    if (log == 2)
+                        SendToAllClients("enemy done");
+                }
+                while (log != 2)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+
+
+
+            });
+        }
+
+
+
+
+
         static private void ReciveFromClients(TcpClient client)
         {
             Task.Factory.StartNew(() =>//принятие писем от клиентов
